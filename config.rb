60a20ae06514d168a3365ece153f065be27dbb5e
gynx <advanceddesigns@gmail.com>
2016-01-05 10:57:13 -0600

    this is the output from sass

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Folder Settings
+
+relative_assests = true
+css_dir = "/css"
+sass_dir = "sass"
+images_dir = "images"
+
+# Preferred Output Style
+output_style = :compressed
+
+
+# Preferred Syntax
+preferred_syntax = :scss
